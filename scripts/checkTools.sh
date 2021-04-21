1912bf6fb4b15679802d33d85cfa04bc44ba07fd
lubenard <lubenard@student.42.fr>
2021-04-21 23:51:23 +0200

    Added mtools as needed command + hide terminal errors

--- scripts/checkTools.sh
+++ scripts/checkTools.sh
@@ -4,11 +4,11 @@ RED="\x1b[31m"
 GREEN="\x1b[32m"
 END="\x1b[0m"
 
-commandToCheck=("nasm" "clang" "ld" "grub-mkrescue" "qemu-system-x86_64" "xorriso")
+commandToCheck=("nasm" "clang" "ld" "mformat" "grub-mkrescue" "qemu-system-x86_64" "xorriso")
 
 for i in "${commandToCheck[@]}"
 do
-	if ! type "$i" > /dev/null; then
+	if ! type "$i" &> /dev/null; then
 		printf "$RED$i$END is not installed\n"
 	else
 		printf "$GREEN$i$END has been found !\n"
