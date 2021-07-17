2d629ec7cc1bb54278b6f41df30257b0060431cf
lubenard <lubenard@student.42.fr>
2021-07-17 18:47:07 +0200

    Reworked the page system

--- srcs/kernel/memory/heap/heap.c
+++ srcs/kernel/memory/heap/heap.c
@@ -6,7 +6,7 @@
 /*   By: lubenard <lubenard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 22:17:41 by lubenard          #+#    #+#             */
-/*   Updated: 2021/07/13 13:48:50 by lubenard         ###   ########.fr       */
+/*   Updated: 2021/07/17 18:38:05 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,14 @@ mem_page_tracking_t *create_new_node_memory(uint32_t addr_low, uint32_t size, ui
 	// We can create a header element right now.
 	if (!(node = (mem_page_tracking_t*)e_kmalloc(sizeof(mem_page_tracking_t), 0, 0)))
 		return 0;
-	(void)addr_low;
-	(void)size;
-	(void)pid;
-	printk(KERN_INFO, "node is at addr %x", &node);
-	//node->addr_low = addr_low;
+	node->prev = 0;
+	node->addr_low = addr_low;
 	//node->addr_high = entry->addr_high;
-	//node->len_low = size;
-	printk(KERN_INFO, "Success alloc");
+	node->len_low = size;
 	//node->len_high = entry->len_high;
-	//node->is_allocated = 1;
-	//node->owner_pid = pid;
-	printk(KERN_INFO, "Success return");
+	node->is_allocated = 1;
+	node->owner_pid = pid;
+	node->next = 0;
 	return node;
 }
 
@@ -47,15 +43,22 @@ mem_page_tracking_t *first_fit_memory(mem_page_tracking_t *lknd_lst, unsigned in
 	// If there is no element in the linked list, all memory is free.
 	if (!lknd_lst) {
 		printk(KERN_INFO, "Creating header linking list node");
-		mem_page_tracking_t *head = create_new_node_memory((uint32_t) 0, size, 0);
-		printk(KERN_INFO, "First node is size %d containing addr %x", head->len_low, head->addr_low);
+		mem_page_tracking_t *head = create_new_node_memory((uint32_t) &endKernel, size, 0);
+		printk(KERN_INFO, "First node is size %d containing addr %x. Created at addr %x", head->len_low, head->addr_low, &head);
 		return head;
 	} else {
 		while (lknd_lst) {
-			if (lknd_lst->addr_low + size < lknd_lst->next->addr_low) {
-				// We can allocate here
+			if (lknd_lst->next) {
+				if (lknd_lst->addr_low + size < lknd_lst->next->addr_low) {
+					// We can allocate here
+					mem_page_tracking_t *node = create_new_node_memory(lknd_lst->addr_low + lknd_lst->len_low, size, 0);
+					printk(KERN_INFO, "New node is size %d containing addr %x. Created at addr %x", node->len_low, node->addr_low, &node);
+					return node;
+				}
+			} else {
+				// Mean we are at the end of memory
 				mem_page_tracking_t *node = create_new_node_memory(lknd_lst->addr_low + lknd_lst->len_low, size, 0);
-				printk(KERN_INFO, "Node is size %d", node->len_low);
+				printk(KERN_INFO, "New node 2 is size %d containing addr %x. Created at addr %x", node->len_low, node->addr_low, &node);
 				return node;
 			}
 			lknd_lst = lknd_lst->next;
--- srcs/kernel/memory/memory.c
+++ srcs/kernel/memory/memory.c
@@ -6,7 +6,7 @@
 /*   By: lubenard <lubenard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 15:47:20 by lubenard          #+#    #+#             */
-/*   Updated: 2021/07/13 13:29:15 by lubenard         ###   ########.fr       */
+/*   Updated: 2021/07/17 18:15:40 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,37 +37,59 @@ uint32_t e_kmalloc(uint32_t size, int align, uint32_t *phys) {
 }
 
 void init_memory(multiboot_info_t *mb_mmap) {
-	page_directory_t *page_directory;
-	uint32_t nframes;
+	uint32_t page_directory[1024] __attribute__((aligned(4096)));
+	uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
+	//page_directory_t *page_directory;
+	//uint32_t nframes;
 
 	multiboot_memory_map_t *map_entry = get_memory_map_from_grub(mb_mmap);
+	(void)map_entry;
+
+	// holds the physical address where we want to start mapping these pages to.
+	// in this case, we want to map these pages to the very beginning of memory
+	unsigned int i;
+
+	//unsigned int j;
+
+	//for (i = )
+	//we will fill all 1024 entries in the table, mapping 4 megabytes
+	for(i = 0; i < 1024; i++) {
+		// As the address is page aligned, it will always leave 12 bits zeroed.
+		// Those bits are used by the attributes ;)
+		first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+	}
+
+	page_directory[0] = ((unsigned int)first_page_table) | 3;
 
 	// Getting the size of the memory via grub.
-	nframes = map_entry->len_low / 0x1000; // 0x1000 -> 4096 (size of 1 page)
+	/*nframes = map_entry->len_low / 0x1000; // 0x1000 -> 4096 (size of 1 page)
 	setFrames((uint32_t *)e_kmalloc(INDEX_FROM_BIT(nframes), 0, 0), nframes);
 
 	// Let's make a page directory.
 	page_directory = (page_directory_t*)e_kmalloc(sizeof(page_directory_t), 1, 0);
 	memset(page_directory, 0, sizeof(page_directory_t));
 
-	unsigned int i = 0;
+	unsigned int i = 0;*/
 
-	for (i = KHEAP_START; i < KHEAP_START+KHEAP_MIN_SIZE; i += 0x1000)
-		get_page(i, 1, page_directory);
+	/*for (i = KHEAP_START; i < KHEAP_START+KHEAP_MIN_SIZE; i += 0x1000)
+		get_page(i, 1, page_directory);*/
 
-	i = 0;
+	/*i = 0;
 	while (i < (uint32_t)placement_address) {
 		// Kernel code is readable but not writeable from userspace.
 		alloc_frame(get_page(i, 1, page_directory), 0, 0);
 		i += 0x1000;
-	}
+	}*/
 
-	for (i = KHEAP_START; i < KHEAP_START+KHEAP_MIN_SIZE; i += 0x1000)
-		alloc_frame( get_page(i, 1, page_directory), 0, 0);
+	/*for (i = KHEAP_START; i < KHEAP_START+KHEAP_MIN_SIZE; i += 0x1000)
+		alloc_frame(get_page(i, 1, page_directory), 0, 0);*/
 
-	enable_paging(&page_directory->tablesPhysical[0]);
-	printk(KERN_INFO, "Paging enabled and working, can begin ram at %x for %d bytes", map_entry->addr_low, map_entry->len_low);
+	enable_paging(page_directory);
+	//enable_paging(&page_directory->tablesPhysical[0]);
+	//printk(KERN_INFO, "Paging enabled and working, can begin ram at %x for %d bytes", map_entry->addr_low, map_entry->len_low);
 	printk(KERN_INFO, "EndKenel says %x, addr_low says %d", placement_address, map_entry->addr_low);
-	printk(KERN_INFO, "ram is now at %x, with a size of %d", placement_address, map_entry->len_low - (placement_address - map_entry->addr_low));
-	first_fit_memory(0, 10);
+	//printk(KERN_INFO, "ram is now at %x, with a size of %d", placement_address, map_entry->len_low - (placement_address - map_entry->addr_low));
+	mem_page_tracking_t *head = first_fit_memory(0, 10);
+	first_fit_memory(head, 4);
 }
