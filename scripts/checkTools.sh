ac9697d6c2ab7aa5fde3e801ca2b6b7fa8996e55
lubenard <lubenard@student.42.fr>
2022-04-14 14:50:08 +0200

    Improved dependencies script

--- scripts/checkTools.sh
+++ scripts/checkTools.sh
@@ -3,14 +3,31 @@
 RED="\x1b[31m"
 GREEN="\x1b[32m"
 END="\x1b[0m"
+YELLOW="\x1b[1;33m"
 
-commandToCheck=("nasm" "clang" "ld" "mformat" "grub-mkrescue" "qemu-system-x86_64" "xorriso")
+requiredTools=("nasm" "clang" "ld" "mformat" "grub-mkrescue" "qemu-system-x86_64" "xorriso")
 
-for i in "${commandToCheck[@]}"
+optionnalTools=("xdotool" "gdb")
+
+requiredSymbol="$RED/!\ $END"
+optionnalSymbol="$YELLOW ~ $END"
+
+printf "$RED- Required -$END\n"
+for i in "${requiredTools[@]}"
 do
 	if ! type "$i" &> /dev/null; then
-		printf "$RED$i$END is not installed\n"
+		printf "$requiredSymbol $RED$i$END is not installed\n"
 	else
-		printf "$GREEN$i$END has been found !\n"
+		printf "$requiredSymbol $GREEN$i$END has been found !\n"
+	fi
+done
+printf "\n"
+printf "$GREEN- Optionnal -$END\n"
+for i in "${optionnalTools[@]}"
+do
+	if ! type "$i" &> /dev/null; then
+		printf "$optionnalSymbol $RED$i$END is not installed\n"
+	else
+		printf "$optionnalSymbol $GREEN$i$END has been found !\n"
 	fi
 done
